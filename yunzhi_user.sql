3e8b14fb981383ccf244472859204b2593925221
kongrp <960139253@qq.com>
2016-05-26 01:50:14 +0800

    data

--- yunzhi_user.sql
+++ yunzhi_user.sql
@@ -1,22 +1,22 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : love
-Source Server Version : 50611
+Source Server         : localhost
+Source Server Version : 50620
 Source Host           : localhost:3306
 Source Database       : wjytest
 
 Target Server Type    : MYSQL
-Target Server Version : 50611
+Target Server Version : 50620
 File Encoding         : 65001
 
-Date: 2016-05-26 17:44:54
+Date: 2016-05-26 01:49:03
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for yunzhi_user
+-- Table structure for `yunzhi_user`
 -- ----------------------------
 DROP TABLE IF EXISTS `yunzhi_user`;
 CREATE TABLE `yunzhi_user` (
@@ -24,7 +24,7 @@ CREATE TABLE `yunzhi_user` (
   `name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of yunzhi_user
@@ -32,3 +32,12 @@ CREATE TABLE `yunzhi_user` (
 INSERT INTO `yunzhi_user` VALUES ('1', 'zhangsan', '1');
 INSERT INTO `yunzhi_user` VALUES ('2', 'lisi', '1');
 INSERT INTO `yunzhi_user` VALUES ('3', 'wangwu', '1');
+INSERT INTO `yunzhi_user` VALUES ('4', 'sunliu', '1');
+INSERT INTO `yunzhi_user` VALUES ('5', '1', '1');
+INSERT INTO `yunzhi_user` VALUES ('6', '2', '2');
+INSERT INTO `yunzhi_user` VALUES ('7', '3', '3');
+INSERT INTO `yunzhi_user` VALUES ('8', '4', '4');
+INSERT INTO `yunzhi_user` VALUES ('9', '6', '6');
+INSERT INTO `yunzhi_user` VALUES ('10', '5', '5');
+INSERT INTO `yunzhi_user` VALUES ('11', '7', '7');
+INSERT INTO `yunzhi_user` VALUES ('12', '8', '8');
